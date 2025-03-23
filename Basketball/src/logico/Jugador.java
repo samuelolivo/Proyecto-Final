0d3c1a15a045c7d88f27252a722cf3fac27868b7
unity <unity@LAPTOP-0A49I65L>
2025-03-23 11:34:57 -0400

    Clases Jugador y Lesion creadas :D

---
+++ Basketball/src/logico/Jugador.java
@@ -0,0 +1,104 @@
+package logico;
+
+import java.util.ArrayList;
+
+public class Jugador {
+	private String id;
+	private String nombre;
+	private float pesoKg;
+	private float alturaCm;
+	private Equipo equipo;
+	private ArrayList<Lesion>misLesiones;
+	private ArrayList<Juego>juegos;
+	private EstJugador estadisticas;
+	private boolean estadoSalud;
+	
+	public Jugador(String id, String nombre, float pesoKg, float alturaCm, Equipo equipo, ArrayList<Lesion> misLesiones,
+			ArrayList<Juego> juegos, EstJugador estadisticas, boolean estadoSalud) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.pesoKg = pesoKg;
+		this.alturaCm = alturaCm;
+		this.equipo = equipo;
+		this.misLesiones = misLesiones;
+		this.juegos = juegos;
+		this.estadisticas = estadisticas;
+		this.estadoSalud = estadoSalud;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public float getPesoKg() {
+		return pesoKg;
+	}
+
+	public void setPesoKg(float pesoKg) {
+		this.pesoKg = pesoKg;
+	}
+
+	public float getAlturaCm() {
+		return alturaCm;
+	}
+
+	public void setAlturaCm(float alturaCm) {
+		this.alturaCm = alturaCm;
+	}
+
+	public Equipo getEquipo() {
+		return equipo;
+	}
+
+	public void setEquipo(Equipo equipo) {
+		this.equipo = equipo;
+	}
+
+	public ArrayList<Lesion> getMisLesiones() {
+		return misLesiones;
+	}
+
+	public void setMisLesiones(ArrayList<Lesion> misLesiones) {
+		this.misLesiones = misLesiones;
+	}
+
+	public ArrayList<Juego> getJuegos() {
+		return juegos;
+	}
+
+	public void setJuegos(ArrayList<Juego> juegos) {
+		this.juegos = juegos;
+	}
+
+	public EstJugador getEstadisticas() {
+		return estadisticas;
+	}
+
+	public void setEstadisticas(EstJugador estadisticas) {
+		this.estadisticas = estadisticas;
+	}
+
+	public boolean isEstadoSalud() {
+		return estadoSalud;
+	}
+
+	public void setEstadoSalud(boolean estadoSalud) {
+		this.estadoSalud = estadoSalud;
+	}
+
+		
+
+}
---
+++ Basketball/src/logico/Lesion.java
@@ -0,0 +1,72 @@
+package logico;
+
+public class Lesion {
+	private String id;
+	private Jugador jugador;
+	private LocalDate fechaLes;
+	private LocalDate fechaRecPrevista;
+	private String descripcionCorta;
+	private boolean estadoRec;
+	
+	public Lesion(String id, Jugador jugador, LocalDate fechaLes, LocalDate fechaRecPrevista, String descripcionCorta,
+			boolean estadoRec) {
+		super();
+		this.id = id;
+		this.jugador = jugador;
+		this.fechaLes = fechaLes;
+		this.fechaRecPrevista = fechaRecPrevista;
+		this.descripcionCorta = descripcionCorta;
+		this.estadoRec = estadoRec;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Jugador getJugador() {
+		return jugador;
+	}
+
+	public void setJugador(Jugador jugador) {
+		this.jugador = jugador;
+	}
+
+	public LocalDate getFechaLes() {
+		return fechaLes;
+	}
+
+	public void setFechaLes(LocalDate fechaLes) {
+		this.fechaLes = fechaLes;
+	}
+
+	public LocalDate getFechaRecPrevista() {
+		return fechaRecPrevista;
+	}
+
+	public void setFechaRecPrevista(LocalDate fechaRecPrevista) {
+		this.fechaRecPrevista = fechaRecPrevista;
+	}
+
+	public String getDescripcionCorta() {
+		return descripcionCorta;
+	}
+
+	public void setDescripcionCorta(String descripcionCorta) {
+		this.descripcionCorta = descripcionCorta;
+	}
+
+	public boolean isEstadoRec() {
+		return estadoRec;
+	}
+
+	public void setEstadoRec(boolean estadoRec) {
+		this.estadoRec = estadoRec;
+	}
+	
+	
+
+}
