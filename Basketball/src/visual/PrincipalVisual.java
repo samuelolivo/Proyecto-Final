914dc3842e6e3a2bb3de420ed230d2c812d1fe3b
olivo <olivo@Olivo>
2025-04-10 13:45:41 -0400

    Done, moviendo botones y graficas

--- Basketball/src/visual/PrincipalVisual.java
+++ Basketball/src/visual/PrincipalVisual.java
@@ -56,10 +56,17 @@ public class PrincipalVisual extends JFrame {
     private JMenu mnUsuario;
     private JMenuItem mntmRegUsuario;
     private JMenuItem mntmListadoUsuario;
-    private JMenuItem mntmRespaldo;
     static Socket sfd = null;
-	static DataInputStream EntradaSocket;
-	static DataOutputStream SalidaSocket;
+    static DataInputStream EntradaSocket;
+    static DataOutputStream SalidaSocket;
+    
+    // Referencias a las gráficas para poder refrescarlas
+    private BarraWinrate gwin;
+    private GraficaBarra grafica;
+    private GraficaEfectividad ge;
+    private JMenu mnOtros;
+    private JMenuItem mntmmRespaldo;
+    private JMenuItem mntmmRefrescarEstadisticas;
 
     /**
      * Launch the application.
@@ -81,33 +88,33 @@ public class PrincipalVisual extends JFrame {
      * Create the frame.
      */
     public PrincipalVisual() {
-    	
-    	addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent e) {
-				FileOutputStream serieOut;
-				ObjectOutputStream serieWrite;
-				
-				File directory = new File("rec/data");
-	            if (!directory.exists()) {
-	                directory.mkdirs();
-	            }
-				
-				try {			
-					serieOut = new FileOutputStream("rec/data/serie.dat");
-					serieWrite = new ObjectOutputStream(serieOut);
-					serieWrite.writeObject(SerieNacional.getInstance());
-				} catch (FileNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				
-			}
-		});
-    	
+        
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                FileOutputStream serieOut;
+                ObjectOutputStream serieWrite;
+                
+                File directory = new File("rec/data");
+                if (!directory.exists()) {
+                    directory.mkdirs();
+                }
+                
+                try {            
+                    serieOut = new FileOutputStream("rec/data/serie.dat");
+                    serieWrite = new ObjectOutputStream(serieOut);
+                    serieWrite.writeObject(SerieNacional.getInstance());
+                } catch (FileNotFoundException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+                
+            }
+        });
+        
 
         setTitle("Serie Nacional de Basketball");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -122,21 +129,21 @@ public class PrincipalVisual extends JFrame {
         
         mntmListadoEquipo = new JMenuItem("Listado");
         mntmListadoEquipo.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent arg0) {
-        		ListadoEquipos listado = new ListadoEquipos();
-        		listado.setVisible(true);
-        		listado.setModal(true);
-        	}
+            public void actionPerformed(ActionEvent arg0) {
+                ListadoEquipos listado = new ListadoEquipos();
+                listado.setVisible(true);
+                listado.setModal(true);
+            }
         });
         mnEquipo.add(mntmListadoEquipo);
         
         mntmRegEquipo = new JMenuItem("Registrar");
         mntmRegEquipo.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		RegEquipo registrar = new RegEquipo(null);
-        		registrar.setVisible(true);
-        		registrar.setModal(true);
-        	}
+            public void actionPerformed(ActionEvent e) {
+                RegEquipo registrar = new RegEquipo(null);
+                registrar.setVisible(true);
+                registrar.setModal(true);
+            }
         });
         mnEquipo.add(mntmRegEquipo);
         mnJugador = new JMenu("  Jugadores  ");
@@ -144,21 +151,21 @@ public class PrincipalVisual extends JFrame {
         
         mntmListadoJugador = new JMenuItem("Listado");
         mntmListadoJugador.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		ListadoJugadores listado = new ListadoJugadores(null);
-        		listado.setVisible(true);
-        		listado.setModal(true);
-        	}
+            public void actionPerformed(ActionEvent e) {
+                ListadoJugadores listado = new ListadoJugadores(null);
+                listado.setVisible(true);
+                listado.setModal(true);
+            }
         });
         mnJugador.add(mntmListadoJugador);
         
         mntmRegJugador = new JMenuItem("Registrar");
         mntmRegJugador.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		RegJugador registrar = new RegJugador(null);
-        		registrar.setVisible(true);
-        		registrar.setModal(true);        		
-        	}
+            public void actionPerformed(ActionEvent e) {
+                RegJugador registrar = new RegJugador(null);
+                registrar.setVisible(true);
+                registrar.setModal(true);              
+            }
         });
         mnJugador.add(mntmRegJugador);
         
@@ -166,11 +173,11 @@ public class PrincipalVisual extends JFrame {
         menuBar.add(mnCalendario);
         mntmListadoJuegos = new JMenuItem("Listado");
         mntmListadoJuegos.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		ListadoJuegos listado = new ListadoJuegos(null);
-        		listado.setVisible(true);
-        		listado.setModal(true);
-        	}
+            public void actionPerformed(ActionEvent e) {
+                ListadoJuegos listado = new ListadoJuegos(null);
+                listado.setVisible(true);
+                listado.setModal(true);
+            }
         });
         mnCalendario.add(mntmListadoJuegos);
         
@@ -179,11 +186,11 @@ public class PrincipalVisual extends JFrame {
         
         mntmIniciarSimulacion = new JMenuItem("Iniciar");
         mntmIniciarSimulacion.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		PsimulacionJuego simulacion = new PsimulacionJuego();
-        		simulacion.setVisible(true);
-        		simulacion.setModal(true); 
-        	}
+            public void actionPerformed(ActionEvent e) {
+                PsimulacionJuego simulacion = new PsimulacionJuego();
+                simulacion.setVisible(true);
+                simulacion.setModal(true); 
+            }
         });
         mnSimulacion.add(mntmIniciarSimulacion);
         
@@ -192,67 +199,78 @@ public class PrincipalVisual extends JFrame {
         
         mntmRegUsuario = new JMenuItem("Registrar");
         mntmRegUsuario.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent arg0) {
-        		RegUser usuario = new RegUser();
-        		usuario.setVisible(true);
-        		usuario.setModal(true);
-        	}
+            public void actionPerformed(ActionEvent arg0) {
+                RegUser usuario = new RegUser();
+                usuario.setVisible(true);
+                usuario.setModal(true);
+            }
         });
         
         mntmListadoUsuario = new JMenuItem("Listado");
         mntmListadoUsuario.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent arg0) {
-        		ListadoUsuarios listado = new ListadoUsuarios();
-        		listado.setVisible(true);
-        		listado.setModal(true);
-        	}
+            public void actionPerformed(ActionEvent arg0) {
+                ListadoUsuarios listado = new ListadoUsuarios();
+                listado.setVisible(true);
+                listado.setModal(true);
+            }
         });
         mnUsuario.add(mntmListadoUsuario);
         mnUsuario.add(mntmRegUsuario);
         
-        mntmRespaldo = new JMenuItem("Respaldo");
-        mntmRespaldo.setIgnoreRepaint(true);
-        mntmRespaldo.setAlignmentX(Component.LEFT_ALIGNMENT);
-        mntmRespaldo.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent arg0) {
+        mnOtros = new JMenu("  Otros  ");
+        menuBar.add(mnOtros);
+        
+        mntmmRespaldo = new JMenuItem("Respaldar");
+        mntmmRespaldo.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
         		try
-			    {
-				      sfd = new Socket("127.0.0.1",9000);
-				      
-				      File directory = new File("rec/data");
-			            if (!directory.exists()) {
-			                directory.mkdirs();
-			            }
-			            
-				      DataInputStream aux = new DataInputStream(new FileInputStream(new File("rec/data/serie.dat")));
-				      SalidaSocket = new DataOutputStream((sfd.getOutputStream()));
-				      int unByte;
-				      try
-				      {
-				    	while ((unByte = aux.read()) != -1){
-				    		SalidaSocket.write(unByte);
-							SalidaSocket.flush();
-				    	}
-				      }
-				      catch (IOException ioe)
-				      {
-				        System.out.println("Error: "+ioe);
-				      }
-				    }
-				    catch (UnknownHostException uhe)
-				    {
-				      System.out.println("No se puede acceder al servidor.");
-				      System.exit(1);
-				    }
-				    catch (IOException ioe)
-				    {
-				      System.out.println("Comunicación rechazada.");
-				      System.exit(1);
-				    }
-				}
+                {
+                      sfd = new Socket("127.0.0.1",9000);
+                      
+                      File directory = new File("rec/data");
+                        if (!directory.exists()) {
+                            directory.mkdirs();
+                        }
+                        
+                      DataInputStream aux = new DataInputStream(new FileInputStream(new File("rec/data/serie.dat")));
+                      SalidaSocket = new DataOutputStream((sfd.getOutputStream()));
+                      int unByte;
+                      try
+                      {
+                        while ((unByte = aux.read()) != -1){
+                            SalidaSocket.write(unByte);
+                            SalidaSocket.flush();
+                        }
+                      }
+                      catch (IOException ioe)
+                      {
+                        System.out.println("Error: "+ioe);
+                      }
+                    }
+                    catch (UnknownHostException uhe)
+                    {
+                      System.out.println("No se puede acceder al servidor.");
+                      System.exit(1);
+                    }
+                    catch (IOException ioe)
+                    {
+                      System.out.println("Comunicación rechazada.");
+                      System.exit(1);
+                    }
+        		OperacionExitosa operacion = new OperacionExitosa();
+        		operacion.setVisible(true);
+        		operacion.setModal(true);
+                }
         });
-        mntmRespaldo.setHorizontalAlignment(SwingConstants.LEFT);
-        menuBar.add(mntmRespaldo);
+        mnOtros.add(mntmmRespaldo);
+        
+        mntmmRefrescarEstadisticas = new JMenuItem("Refrescar Estad\u00EDsticas");
+        mntmmRefrescarEstadisticas.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		refrescarGraficas();
+        	}
+        });
+        mnOtros.add(mntmmRefrescarEstadisticas);
 
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -263,33 +281,68 @@ public class PrincipalVisual extends JFrame {
         contentPane.add(panel);
         panel.setLayout(null);
         
-             //bara 2 win
-             BarraWinrate gwin = new BarraWinrate();
+             // Bara 2 win
+             gwin = new BarraWinrate();
              gwin.setBounds(12, 458, 1228, 150);
              panel.add(gwin);
              
-             //bara 1 tops3
-             GraficaBarra grafica = new GraficaBarra();
-             grafica.setBounds(226, 16, 453, 429);
+             // Bara 1 tops3
+             grafica = new GraficaBarra();
+             grafica.setBounds(12, 16, 667, 429);
              panel.add(grafica);
              
-             //bara 3 top10
-             GraficaEfectividad ge = new GraficaEfectividad();
+             // Bara 3 top10
+             ge = new GraficaEfectividad();
              ge.setBounds(702, 13, 538, 429);
              panel.add(ge);
         
-
              User miUser = SerieNacional.getLoginUser();
              if (miUser != null)
              {
-	             if(!miUser.getTipo().equals("Administrador"))
-	             {
-	            	 mnUsuario.setVisible(false);
-	            	 mntmRegJugador.setVisible(false);
-	            	 mntmRegEquipo.setVisible(false);
-	            	 mntmRegUsuario.setVisible(false);
-	            	 mntmListadoUsuario.setVisible(false);
-	             }
+                 if(!miUser.getTipo().equals("Administrador"))
+                 {
+                     mnUsuario.setVisible(false);
+                     mntmRegJugador.setVisible(false);
+                     mntmRegEquipo.setVisible(false);
+                     mntmRegUsuario.setVisible(false);
+                     mntmListadoUsuario.setVisible(false);
+                 }
             }
+    }
+    
+    /**
+     * Método para refrescar las gráficas
+     */
+    private void refrescarGraficas() {
+        System.out.println("Refrescando gráficas de estadísticas...");
+        
+        // Elimina los paneles actuales
+        contentPane.removeAll();
+        
+        // Crea un nuevo panel
+        JPanel panel = new JPanel();
+        contentPane.add(panel);
+        panel.setLayout(null);
+        
+        // Crea nuevas instancias de las gráficas
+        gwin = new BarraWinrate();
+        gwin.setBounds(12, 458, 1228, 150);
+        panel.add(gwin);
+        
+        grafica = new GraficaBarra();
+        grafica.setBounds(226, 16, 453, 429);
+        panel.add(grafica);
+        
+        ge = new GraficaEfectividad();
+        ge.setBounds(702, 13, 538, 429);
+        panel.add(ge);
+        
+        // Actualiza la visualización
+        panel.revalidate();
+        panel.repaint();
+        contentPane.revalidate();
+        contentPane.repaint();
+        
+        System.out.println("Estadísticas refrescadas con éxito");
     }
 }
